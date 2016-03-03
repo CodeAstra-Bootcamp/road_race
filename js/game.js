b3146d4a8ef3e3bc63729fdc10d00dc3ad84d2e5
B V Satyaram <bvsatyaram@gmail.com>
2016-03-03 13:17:07 +0530

    Move car

--- js/game.js
+++ js/game.js
@@ -11,20 +11,32 @@ var Game = {
     this.enemyTop += 1;
     this.enemy.css("top", this.enemyTop + "vmin");
   },
+  moveRight: function() {
+    var _this = Game;
+    if (_this.carLeft < 40) {
+      _this.carLeft += 10;
+      _this.car.css("left", _this.carLeft + "vmin");
+    }
+  },
+  moveLeft: function() {
+    var _this = Game;
+    if (_this.carLeft >= 10) {
+      _this.carLeft -= 10;
+      _this.car.css("left", _this.carLeft + "vmin");
+    }
+  },
   init: function() {
     this.enemy = $('#enemy_car');
+    this.car = $('#my_car');
+    this.carLeft = 0;
     this.resetEnemy();
     var _this = this;
     setInterval(function(){
       _this.moveEnemy();
     }, 30);
 
-    $(document).bind('keydown', 'right', function() {
-      console.log("Right key pressed")
-    });
-    $(document).bind('keydown', 'left', function() {
-      console.log("Left key pressed")
-    });
+    $(document).bind('keydown', 'right', _this.moveRight);
+    $(document).bind('keydown', 'left',  _this.moveLeft);
   }
 }
 
