ab3fafb7d5b12bf8da593fedf39c5fbe854603ab
B V Satyaram <bvsatyaram@gmail.com>
2016-03-03 14:14:22 +0530

    Increase level with time

--- js/game.js
+++ js/game.js
@@ -1,5 +1,13 @@
 var Game = {
+  increaseLevel: function() {
+    this.enemySpeed *= 1.2;
+    this.scoreSpeed *= 1.2;
+  },
   resetEnemy: function() {
+    this.carsCount += 1;
+    if (this.carsCount % 2 == 0) {
+      this.increaseLevel();
+    }
     this.enemyTop = -20;
     this.enemyLeft = Math.floor(Math.random()*5) * 10;
     this.enemy.css("left", this.enemyLeft + "vmin");
@@ -16,15 +24,15 @@ var Game = {
     }
   },
   incrementScore: function() {
-    this.score += 1;
-    $('#score').html(this.score);
+    this.score += this.scoreSpeed;
+    $('#score').html(Math.floor(this.score));
   },
   moveEnemy: function() {
     this.incrementScore();
     if (this.enemyTop > 100) {
       this.resetEnemy();
     }
-    this.enemyTop += 1;
+    this.enemyTop += this.enemySpeed;
     this.enemy.css("top", this.enemyTop + "vmin");
     this.checkCollision();
   },
@@ -43,9 +51,12 @@ var Game = {
     }
   },
   init: function() {
+    this.enemySpeed = 1;
+    this.scoreSpeed = 1;
     this.enemy = $('#enemy_car');
     this.car = $('#my_car');
     this.carLeft = 0;
+    this.carsCount = 0;
     this.resetEnemy();
     this.score = 0;
     var _this = this;
